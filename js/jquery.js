2fc63cc22f6cdef278834979270e98b59b1e9837
Grace Oswal <graceoswal88@gmail.com>
2020-09-07 14:14:27 +0300

    add codes to jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -17,3 +17,32 @@
 
 // Pass this if window is not defined yet
 } )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+    "use strict";
+
+    var arr = [];
+
+    var document = window.document;
+
+    var getProto = Object.getPrototypeOf;
+
+    var slice = arr.slice;
+
+    var concat = arr.concat;
+
+    var push = arr.push;
+
+    var indexOf = arr.indexOf;
+
+    var class2type = {};
+
+    var toString = class2type.toString;
+
+    var hasOwn = class2type.hasOwnProperty;
+
+    var fnToString = hasOwn.toString;
+
+    var ObjectFunctionString = fnToString.call( Object );
+
+    var support = {};
+
+    var isFunction = function isFunction( obj ) {
