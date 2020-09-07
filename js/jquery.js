6240383080e6b2b7f60c9130628547c5d487dc8d
Grace Oswal <graceoswal88@gmail.com>
2020-09-07 14:16:17 +0300

    add codes to jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -46,3 +46,56 @@
     var support = {};
 
     var isFunction = function isFunction( obj ) {
+        return typeof obj === "function" && typeof obj.nodeType !== "number";
+    };
+
+
+    var isWindow = function isWindow( obj ) {
+        return obj != null && obj === obj.window;
+    };
+
+
+
+
+    var preservedScriptAttributes = {
+        type: true,
+        src: true,
+        nonce: true,
+        noModule: true
+    };
+
+    function DOMEval( code, node, doc ) {
+        doc = doc || document;
+
+        var i, val,
+            script = doc.createElement( "script" );
+
+        script.text = code;
+        if ( node ) {
+            for ( i in preservedScriptAttributes ) {
+                 val = node[ i ] || node.getAttribute && node.getAttribute( i );
+                if ( val ) {
+                    script.setAttribute( i, val );
+                }
+            }
+        }
+        doc.head.appendChild( script ).parentNode.removeChild( script );
+    }
+
+
+    function toType( obj ) {
+        if ( obj == null ) {
+            return obj + "";
+        }
+
+        // Support: Android <=2.3 only (functionish RegExp)
+        return typeof obj === "object" || typeof obj === "function" ?
+            class2type[ toString.call( obj ) ] || "object" :
+            typeof obj;
+    }
+    var
+        version = "3.4.1",
+
+        // Define a local copy of jQuery
+        jQuery = function( selector, context ) {
+            
